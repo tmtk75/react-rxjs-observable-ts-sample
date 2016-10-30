c73322e31e48bdd7ed71254ec732b10b40bdc877
Tomotaka Sakuma <ktmtmks@gmail.com>
2016-10-30 17:52:13 +0900

    -

--- src/index.tsx
+++ src/index.tsx
@@ -19,6 +19,7 @@ class App extends React.Component<any, any> {
     super(props);
     this.state = {
       username: "",
+      password: "",
     }
   }
   render() {
@@ -30,6 +31,13 @@ class App extends React.Component<any, any> {
           floatingLabelText="username"
           value={this.state.username}
           onChange={(e: React.FormEvent<TextField>) => this.setState({username: (e.target as any).value})}
+          />
+        <TextField
+          type="password"
+          name="password"
+          floatingLabelText="password"
+          value={this.state.password}
+          onChange={(e: React.FormEvent<TextField>) => this.setState({password: (e.target as any).value})}
           />
         <FlatButton
           label="sign up"
