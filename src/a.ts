db155cd51b0f4d3165492b3e5a1ebdcd0dad2f10
Tomotaka Sakuma <ktmtmks@gmail.com>
2016-10-30 15:59:30 +0900

    -

--- src/a.ts
+++ src/a.ts
@@ -4,4 +4,9 @@ const a = Rx.Observable.range(1, 10)
 const b = Rx.Observable.range(1, 10).map(x => x * 2)
 
 a.zip(b)
-  .subscribe(console.log)
+ .subscribe(console.log)
+
+Rx.Observable.combineLatest(a, b, (a: any, b: any) => {
+    return 1
+  })
+ .subscribe(console.log)
